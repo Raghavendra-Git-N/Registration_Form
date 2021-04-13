0b05a7122436a24b29b6a0dbe8d6cb024c750d4d
raghavendra <me076off@gmail.com>
2021-04-13 17:26:29 +0530

    vendor.js

---
+++ script/vendor.js
@@ -0,0 +1,12 @@
+const userName = document.getElementById('username');
+const password = document.getElementById('password');
+
+const firstName = document.getElementById('fname');
+const lastName = document.getElementById('lname');
+const emailId = document.getElementById('email');
+const password = document.getElementById('password');
+const phoneNumber = document.getElementById('PhoneNo');
+const dob = document.getElementById('DOB');
+const female = document.getElementById('female');
+const male = document.getElementById('male');
+const other = document.getElementById('others');
